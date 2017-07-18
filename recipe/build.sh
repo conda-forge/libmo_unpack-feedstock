469b8bb9f1bc8274d637a9732a0f8aac833f0dd6
Filipe Fernandes <ocefpaf@gmail.com>
2017-07-18 13:05:22 -0300

    update t 3.1.2

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,26 +1,13 @@
 #!/bin/bash
 
-if [[ $(uname) == 'Darwin' ]]; then
-  export LIBRARY_SEARCH_VAR=DYLD_FALLBACK_LIBRARY_PATH
-elif [[ $(uname) == 'Linux' ]]; then
-  export LIBRARY_SEARCH_VAR=LD_LIBRARY_PATH
-fi
+export CFLAGS="-I$PREFIX/include $CFLAGS"
 
+mkdir build && cd build
 
-mkdir m4
-
-# Patch the configure.ac as per https://lists.ubuntu.com/archives/fwts-devel/2013-June/003391.html
-sed -i".ac" -e 's/AM_PROG_AR/m4_ifdef([AM_PROG_AR], [AM_PROG_AR])/g' configure.ac
-autoreconf --install
-
-CFLAGS="-O3 -mfpmath=sse -msse $CFLAGS"
-
-if [[ $(uname) == Darwin ]]
-then
-    CFLAGS="$CFLAGS -D_DARWIN_SOURCE"
-fi
-./configure --prefix=$PREFIX CFLAGS="$CFLAGS"
+cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
+      -D CMAKE_INSTALL_LIBDIR:PATH=$PREFIX/lib \
+      $SRC_DIR
 
 make
-eval ${LIBRARY_SEARCH_VAR}=$PREFIX/lib make check
+make test
 make install
